389df271d55364bb5593380cd2340150af993bf6
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-06-13 15:59:31 -0500

    Todos and Projects components changed to Dumb components

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ProjectItem from './ProjectItem';
 
-class Projects extends Component {
-  deleteProject(id) {
-    this.props.onDelete(id);
-  }
-  render() {
-    const projectItems = this.props.projects
-      ? this.props.projects.map(project => (
-          <ProjectItem
-            onDelete={this.deleteProject.bind(this)}
-            key={project.title}
-            project={project}
-          />
-        ))
-      : '';
-    return (
-      <div className="Projects">
-        <h3>Lastest Projects</h3>
-        {projectItems}
-      </div>
-    );
-  }
-}
-
+const Projects = ({ projects, onDelete }) => {
+  const projectItems = projects
+    ? projects.map(project => (
+        <ProjectItem
+          onDelete={() => onDelete(project.id)}
+          key={project.title}
+          project={project}
+        />
+      ))
+    : '';
+  return (
+    <div className="Projects">
+      <h3>Lastest Projects</h3>
+      {projectItems}
+    </div>
+  );
+};
 export default Projects;
--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TodoItem from './TodoItem';
 
-class Todos extends Component {
-  render() {
-    const todoItems = this.props.todos
-      ? this.props.todos.map(todo => <TodoItem key={todo.title} todo={todo} />)
-      : '';
-    return (
-      <div className="Todos">
-        <h3>Todo List</h3>
-        {todoItems}
-      </div>
-    );
-  }
-}
+const Todos = ({ todos }) => {
+  const todoItems = todos
+    ? todos.map(todo => <TodoItem key={todo.title} todo={todo} />)
+    : '';
+  return (
+    <div className="Todos">
+      <h3>Todo List</h3>
+      {todoItems}
+    </div>
+  );
+};
 
 export default Todos;
